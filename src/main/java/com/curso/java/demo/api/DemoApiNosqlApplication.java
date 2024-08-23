712ae7b39c0b9d153bc91d7ecb7dc4928a257393
victorSsantos <victor.ssantos089@gmail.com>
2024-08-23 09:19:30 -0300

    init commit

---
+++ src/main/java/com/curso/java/demo/api/DemoApiNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.curso.java.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/java/demo/api/DemoApiNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.java.demo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
